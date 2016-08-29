7bbf1e4543286e0c6b929f65f358b2714c0b43e2
BEN SMITH <benjamin.holtzman.smith@gmail.com>
2016-08-29 12:52:57 +0200

    use sendFile to serve root index html

--- website/server.js
+++ website/server.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.static('.'));
 
 app.get('/', function (req, res) {
-  res.send('intex.html');
+  res.sendFile('index.html', {root: __dirname });
 });
 
 app.listen(3000, function () {
-  console.log('Starting Server');
+  console.log('Starting Server on: http://127.0.0.1:3000/');
 });
