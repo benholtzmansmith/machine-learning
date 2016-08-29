9d958899cee413b62145389718c50cfc1325ecc5
BEN SMITH <benjamin.holtzman.smith@gmail.com>
2016-08-29 13:22:47 +0200

    adding exress end point for training

--- website/js/angular/machineLearningApp.js
+++ website/js/angular/machineLearningApp.js
@@ -1,6 +1,13 @@
 angular.module('machineLearningApp', [])
-  .controller('TrainingController', function($scope) {
-    $scope.train = function(){
-      console.log("running")
-    }
+  .controller('TrainingController', function($scope, $http) {
+    $scope.train = function(path){
+		$http.get('/train').then(
+			function() {
+				$scope.result = "Training model!"      
+			},
+			function(){
+				$scope.result = "Failed to train model!"
+			}
+		)
+	}
   });
